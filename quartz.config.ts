d56c1487c49629e2e54cfb31bbdc54efc59015c5
alexofrhodes <anastasioualex@gmail.com>
2024-07-27 14:08:03 +0300

    Quartz sync: Jul 27, 2024, 2:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "alexofrhodes.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
